98639142b8fa022ef28fc78b182e2c579e7d26b7
Alethea Cary <aletheacary@gmail.com>
2019-07-05 00:14:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,7 +6,15 @@ class App < Sinatra::Base
     erb :newteam
   end
   
-  get '/team' do 
+  post '/team' do 
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    
     erb :team
   end
 
